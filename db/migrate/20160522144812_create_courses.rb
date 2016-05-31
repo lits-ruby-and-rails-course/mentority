7c6a513867bd76bbf9fb2f540221bee5405ea613
Olexandr Molodchenko <oleksandr@innocode.no>
2016-05-31 19:07:28 +0300

    add sourse model

--- db/migrate/20160522144812_create_courses.rb
+++ db/migrate/20160522144812_create_courses.rb
@@ -1,9 +1,9 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
+      t.text :title
       t.text :description
       t.integer :user_id, index: true
-      t.timestamps null: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522144812) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "courses", force: :cascade do |t|
-    t.text     "description"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
 
 end
