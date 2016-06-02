e48e7a83a7c5096ea732a7e805dad41bd602a0aa
Taras Kunch <taras.kunch@gmail.com>
2016-06-02 19:30:57 +0300

    Add profile model
    add index and new columns to profile

---
+++ db/migrate/20160601213945_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+
+      t.integer :user_id, index: true, unique: true, foreign_key: true
+      t.attachment  :avatar
+      t.date    :birthday
+      t.text    :introduction
+      t.string  :user_email
+      t.string  :user_phone
+      t.timestamps null: false
+    end
+  end
+end
