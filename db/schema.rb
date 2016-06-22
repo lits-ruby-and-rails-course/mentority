899561ffa3b469ca0d8ae6a37296fa24cb4cb38e
darii <yarusdan@gmail.com>
2016-06-22 19:18:18 +0300

    authenticate_admin_user

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601213945) do
+ActiveRecord::Schema.define(version: 20160622134602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "courses", force: :cascade do |t|
     t.text     "title"
@@ -44,7 +59,7 @@ ActiveRecord::Schema.define(version: 20160601213945) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.date     "birthday"
-    t.text     "personal_info"
+    t.text     "introduction"
     t.string   "user_email"
     t.string   "user_phone"
     t.datetime "created_at",          null: false
