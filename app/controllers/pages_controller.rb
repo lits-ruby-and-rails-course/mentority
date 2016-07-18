1132a36944d684666104ffd8658d8bf8e4597a70
Pavlo Cherniovyi <pavlo.cherniovyi@ralabs.org>
2016-07-18 18:50:56 +0300

    some changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to profile_path(current_user.id)
+      redirect_to profile_path(current_user)
     end
   end
 end
